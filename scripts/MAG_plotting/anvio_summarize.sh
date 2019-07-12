cbbe2f15663802df98ddd1e5a5d626d5a73fcf06
David Galambos <galambosd@carleton.edu>
2019-07-12 00:10:41 -0500

    rework folders

--- scripts/MAG_plotting/anvio_summarize.sh
+++
@@ -1,8 +0,0 @@
-for S in $@
-do
-cd $S
-anvi-import-collection -c *external_calls.db -p SAMPLES-MERGED-RNA_v3/PROFILE.db -C ${S}_bins_v232 ~/file_org/MAG_data/bin_collections/${S}*.txt
-anvi-summarize -c *external_calls.db -p SAMPLES-MERGED-RNA_v3/PROFILE.db -C ${S}_bins_v232 -o SUMMARY_${S}_bins_RNA
-cd ..
-done
-
